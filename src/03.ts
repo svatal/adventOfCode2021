d5ec8ad1a7d1ab2bd67184676f536c33473e26ae
svatal <swata@centrum.cz>
2021-12-02 06:20:03 +0100

    template update

--- src/03.ts
+++ src/03.ts
@@ -1,5 +1,5 @@
-// import { testInput as input } from "./02-input";
-import { input } from "./02-input";
+// import { testInput as input } from "./03-input";
+import { input } from "./03-input";
 
 export function doIt() {
   const parsed = input.split(`\n`).map(
