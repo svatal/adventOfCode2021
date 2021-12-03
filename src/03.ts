4c73721377d7bd99527b682b9c07bc7c0af7193f
svatal <swata@centrum.cz>
2021-12-03 09:12:39 +0100

    03 refactored

--- src/03.ts
+++ src/03.ts
@@ -1,62 +1,49 @@
 // import { testInput as input } from "./03-input";
 import { input } from "./03-input";
 
+type Bit = "1" | "0";
+
 export function doIt() {
-  const parsed = input.split(`\n`).map(
-    (line) => line.split("") //
-  );
-  const first = parsed.reduce((prev, c) => {
-    c.forEach((v, i) => {
-      if (v === "1") prev[i] = (prev[i] ?? 0) + 1;
-    });
-    return prev;
-  }, [] as number[]);
+  const parsed = input.split(`\n`).map((line) => line.split("") as Bit[]);
+  const bitCounts = getBitCounts(parsed);
   console.log(
-    gama(first, parsed.length) * epsilon(first, parsed.length),
-    getUnique(parsed, (v, com, i, len) =>
-      com[i] * 2 >= len ? v[i] === "1" : v[i] === "0"
-    ) *
-      getUnique(parsed, (v, com, i, len) =>
-        com[i] * 2 < len ? v[i] === "1" : v[i] === "0"
-      )
+    getCommonBits(bitCounts, parsed.length, true) *
+      getCommonBits(bitCounts, parsed.length, false),
+    getUnique(parsed, true) * getUnique(parsed, false)
   );
 }
 
-function commonity(vals: string[][]) {
-  return vals.reduce((prev, c) => {
-    c.forEach((v, i) => {
-      if (v === "1") prev[i] = (prev[i] ?? 0) + 1;
+function getBitCounts(values: Bit[][]) {
+  return values.reduce((prev, c) => {
+    c.forEach((bit, i) => {
+      if (bit === "1") prev[i] = (prev[i] ?? 0) + 1;
     });
     return prev;
   }, [] as number[]);
 }
 
-function getUnique(
-  vals: string[][],
-  cond: (v: string[], com: number[], i: number, len: number) => boolean
-) {
-  let second = [...vals];
-  while (second.length > 1) {
-    const com = commonity(second);
-    const i = com.findIndex((v) => v < second.length && v > 0);
-    second = second.filter((v) => cond(v, com, i, second.length));
+function getUnique(values: Bit[][], findMost: boolean) {
+  let candidates = [...values];
+  while (candidates.length > 1) {
+    const bitCounts = getBitCounts(candidates);
+    const i = bitCounts.findIndex(
+      (count) => count < candidates.length && count > 0
+    );
+    const expectedBitValue = getBit(bitCounts[i], candidates.length, findMost);
+    candidates = candidates.filter((v) => v[i] === expectedBitValue);
   }
-  return toNum(second[0]);
+  return toNum(candidates[0]);
 }
 
-function gama(vs: number[], len: number) {
-  return vs
-    .map<number>((v) => (v * 2 > len ? 0 : 1))
-    .reduce((prev, c) => prev * 2 + c, 0);
+function getBit(onesCount: number, totalCount: number, findMost: boolean): Bit {
+  const mostZeroes = onesCount * 2 < totalCount;
+  return mostZeroes !== findMost ? "1" : "0";
 }
 
-function epsilon(vs: number[], len: number) {
-  return vs
-    .map<number>((v) => (v * 2 < len ? 0 : 1))
-    .reduce((prev, c) => prev * 2 + c, 0);
+function getCommonBits(bitCounts: number[], len: number, findMost: boolean) {
+  return toNum(bitCounts.map((c) => getBit(c, len, findMost)));
 }
 
 function toNum(vs: string[]) {
-  console.log(vs);
   return vs.reduce((prev, c) => prev * 2 + (c === "1" ? 1 : 0), 0);
 }
