d3c2f74127d5da1ef5ec421714419ef689dbcea3
svatal <swata@centrum.cz>
2021-12-06 08:33:02 +0100

    06 refactored

--- src/06.ts
+++ src/06.ts
@@ -5,24 +5,18 @@ export function doIt() {
   const parsed = input
     .split(`,`)
     .map((line) => +line)
-    .reduce(
-      (p, c) => {
-        p[c]++;
-        return p;
-      },
-      [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    );
+    .reduce((p, c) => {
+      p[c]++;
+      return p;
+    }, new Array(9).fill(0));
   console.log(runSimulation(parsed, 80), runSimulation(parsed, 256));
 }
 
 function runSimulation(state: number[], steps: number) {
   for (let step = 0; step < steps; step++) {
-    const newState: number[] = [];
-    for (let i = 1; i <= 8; i++) {
-      newState[i - 1] = state[i];
-    }
-    newState[6] += state[0];
-    newState[8] = state[0];
+    const [spawn, ...newState] = state;
+    newState[6] += spawn;
+    newState[8] = spawn;
     state = newState;
   }
   return state.reduce((a, b) => a + b);
