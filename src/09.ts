4295f991c10336b702665180597491d195cf788d
svatal <swata@centrum.cz>
2021-12-09 17:58:26 +0100

    09 refactored

--- src/09.ts
+++ src/09.ts
@@ -1,88 +1,64 @@
 // import { testInput as input } from "./09-input";
-import { posix } from "path/posix";
 import { input } from "./09-input";
-import { log } from "./utils";
+import {
+  neighbors4,
+  existIn,
+  valueInMap,
+  IPosition,
+  posFromString,
+  posToString,
+} from "./utils/position";
 
 export function doIt() {
-  const parsed = input.split(`\n`).map(
-    (line) => line.split("").map((c) => +c) //
-  );
+  const parsed = input.split(`\n`).map((line) => line.split("").map((h) => +h));
   const first = parsed.reduce(
-    (p, l, il) =>
+    (p, line, y) =>
       p +
-      l.reduce((p, h, ih) => {
-        const isLow =
-          (ih === 0 || l[ih - 1] > h) &&
-          (ih === l.length - 1 || l[ih + 1] > h) &&
-          (il === 0 || parsed[il - 1][ih] > h) &&
-          (il === parsed.length - 1 || parsed[il + 1][ih] > h);
-
-        return p + (isLow ? h + 1 : 0);
-      }, 0),
+      line.reduce(
+        (p, h, x) => p + (neighborsAreHigher(parsed, { x, y }) ? h + 1 : 0),
+        0
+      ),
     0
   );
-  const second = parsed.reduce(
-    (p, l, il) => [
-      ...p,
-      ...l.reduce((p, h, ih) => {
-        const isLow =
-          (ih === 0 || l[ih - 1] > h) &&
-          (ih === l.length - 1 || l[ih + 1] > h) &&
-          (il === 0 || parsed[il - 1][ih] > h) &&
-          (il === parsed.length - 1 || parsed[il + 1][ih] > h);
+  const second = parsed
+    .reduce(
+      (p, l, y) => [
+        ...p,
+        ...l.reduce(
+          (p, h, x) =>
+            neighborsAreHigher(parsed, { x, y })
+              ? [...p, getBasinSize(parsed, { y, x })]
+              : p,
+          [] as number[]
+        ),
+      ],
+      [] as number[]
+    )
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a * b, 1);
 
-        return isLow ? [...p, countBasin(parsed, { y: il, x: ih })] : p;
-      }, [] as number[]),
-    ],
-    [] as number[]
-  );
-  console.log(
-    first,
-    second
-      .sort((a, b) => a - b)
-      .map(log)
-      .filter((n, i) => i > second.length - 4)
-      .map(log)
-      .reduce((a, b) => a * b, 1)
-  );
+  console.log(first, second);
 }
 
-function toString(p: { x: number; y: number }) {
-  const { x, y } = p;
-  return `${x}|${y}`;
+function neighborsAreHigher(map: number[][], pos: IPosition) {
+  const val = valueInMap(map)(pos)!;
+  return neighbors4(pos)
+    .filter(existIn(map))
+    .map(valueInMap(map))
+    .every((nh) => nh > val);
 }
 
-function fromString(s: string) {
-  const [x, y] = s.split("|").map((v) => +v);
-  return { x, y };
-}
-
-function neighbors(
-  p: { x: number; y: number } /*, xLen: number, yLen: number*/
-) {
-  const { x, y } = p;
-  return [
-    { x: x + 1, y },
-    { x: x - 1, y },
-    { x, y: y + 1 },
-    { x, y: y - 1 },
-  ]; //.filter(p => p.x >=0 && p.y >=0 && p.x < xLen && p.y < yLen );
-}
-
-function countBasin(map: number[][], p: { x: number; y: number }): number {
-  const edge = new Set([toString(p)]);
+function getBasinSize(map: number[][], p: IPosition): number {
+  const edge = new Set([posToString(p)]);
   const inner = new Set<string>();
   while (edge.size > 0) {
     edge.forEach((pointS) => {
-      const point = fromString(pointS);
-      neighbors(point)
-        .filter(
-          (n) =>
-            map[n.y] !== undefined &&
-            map[n.y][n.x] !== undefined &&
-            map[n.y][n.x] < 9
-        )
-        .map(toString)
+      const point = posFromString(pointS);
+      neighbors4(point)
+        .filter(existIn(map))
+        .filter((n) => valueInMap(map)(n) < 9)
+        .map(posToString)
         .filter((n) => !edge.has(n) && !inner.has(n))
         .forEach((n) => edge.add(n));
       edge.delete(pointS);
--- src/utils/uniques.ts
+++ src/utils/uniques.ts
@@ -1,18 +1,3 @@
-export function log<T>(value: T, index: number, array: T[]) {
-  if (index === 0) console.log(array);
-  return value;
-}
-
-export function logi<T>(filter: (value: T, index: number) => boolean) {
-  return (value: T, index: number, array: T[]) => {
-    if (index === 0) console.log("----");
-
-    if (filter(value, index)) console.log(index, value);
-    if (index === array.length - 1) console.log("----");
-    return value;
-  };
-}
-
 /*
 const input = [
   { id: 1, vals: [1, 2] },  // unique in third round (after removing 3 and 1)
