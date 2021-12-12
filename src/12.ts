346708a6961e9697ea5f463e11ff02ca494eef3e
svatal <swata@centrum.cz>
2021-12-12 10:27:51 +0100

    12 refactored

--- src/12.ts
+++ src/12.ts
@@ -8,44 +8,30 @@ export function doIt() {
     p[b] = [...(p[b] || []), a];
     return p;
   }, {} as Record<string, string[]>);
-  const first = getPaths(parsed, "start", []);
-  const second = getPaths2(parsed, "start", [], false);
+  const first = getPaths(parsed, "start", [], false);
+  const second = getPaths(parsed, "start", [], true);
   console.log(first, second);
 }
 
 function getPaths(
   maze: Record<string, string[]>,
   from: string,
-  forbidden: string[]
-): number {
-  const isSmall = from === from.toLowerCase();
-  return maze[from]
-    .filter((c) => forbidden.indexOf(c) == -1)
-    .reduce((p, c) => {
-      if (c === "end") return p + 1;
-      return p + getPaths(maze, c, isSmall ? [...forbidden, from] : forbidden);
-    }, 0);
-}
-
-function getPaths2(
-  maze: Record<string, string[]>,
-  from: string,
   smallVisited: string[],
-  visitedTwice: boolean
+  canVisitSmallAgain: boolean
 ): number {
   const isSmall = from === from.toLowerCase();
   return maze[from]
-    .filter((c) => !visitedTwice || smallVisited.indexOf(c) == -1)
+    .filter((c) => canVisitSmallAgain || smallVisited.indexOf(c) === -1)
     .reduce((p, c) => {
       if (c === "end") return p + 1;
       if (c === "start") return p;
       return (
         p +
-        getPaths2(
+        getPaths(
           maze,
           c,
           isSmall ? [...smallVisited, from] : smallVisited,
-          visitedTwice || smallVisited.indexOf(c) >= 0
+          canVisitSmallAgain && smallVisited.indexOf(c) === -1
         )
       );
     }, 0);
